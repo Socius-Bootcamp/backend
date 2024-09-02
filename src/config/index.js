a0c6cda28f0e13f17794e7759e96d8622615f8ff
JuanSphereOne <juan@sphereone.xyz>
2024-09-02 19:58:00 -0300

    remove timestamp from models

--- src/config/index.js
+++ src/config/index.js
@@ -5,6 +5,9 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
   pool: dbConfig.pool,
+  define: {
+    timestamps: false
+  }
 });
 
 const db = {};
